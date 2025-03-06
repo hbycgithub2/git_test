764f1117a4adfe589ef278be54fb76e6d233951b
hbycgithub2 <1311768852@qq.com>
2025-03-06 17:47:38 +0800

    first  push

--- src/main/java/com/gitTest/Main.java
+++ src/main/java/com/gitTest/Main.java
@@ -3,5 +3,6 @@ package com.gitTest;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! git_test");
+        System.out.println("Hello world! git_test push");
     }
 }
